9351d9031b0af222db10f4c70852f5ed3c1a0486
Alexander SABOURET <asaboure@e2r11p14.clusters.42paris.fr>
2022-05-16 18:55:10 +0200

    d

---
+++ vector.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/16 17:24:06 by asaboure          #+#    #+#             */
+/*   Updated: 2022/05/16 18:19:01 by asaboure         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+namespace	ft{
+
+	template<typename T>
+	class vector
+	{
+	private:
+
+	public:
+		vector();														//CANON
+		~vector();														//CANON
+	};
+}
+
+#endif
