6ca3c1e8c226b1dda515ebc6f0d0b7ab8e28a0ec
Alexander SABOURET <asaboure@e1r11p19.clusters.42paris.fr>
2022-05-20 15:28:51 +0200

    d

--- main.cpp
+++ main.cpp
@@ -6,15 +6,27 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 13:43:58 by asaboure          #+#    #+#             */
-/*   Updated: 2022/05/20 14:36:56 by asaboure         ###   ########.fr       */
+/*   Updated: 2022/05/20 15:26:42 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <vector>
 #include "vector.hpp"
+#include <iostream>
 
 int	main(){
+	int	i = 0;
+	
 	std::vector<int>	vec;
 	ft::vector<int>		ft_vec;
 	
+	std::vector<int>	vec2(4);
+	ft::vector<int>		ft_vec2(4);
+
+	for (std::vector<int>::iterator it = vec2.begin(); it != vec2.end(); it++)
+		std::cout << i++ << std::endl;
+	i = 0;
+	std::cout << std::endl;
+	for (ft::vector<int>::iterator it = ft_vec2.begin(); it != ft_vec2.end(); it++)
+		std::cout << i++ << std::endl;
 }
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 13:35:49 by asaboure          #+#    #+#             */
-/*   Updated: 2022/05/20 14:36:15 by asaboure         ###   ########.fr       */
+/*   Updated: 2022/05/20 15:25:57 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,54 @@
 # define VECTOR_HPP
 
 namespace ft
-{
+{	
 	template<typename T>
 	class vector
-	{
-	private:
-		T	*array;
+	{	
 	public:
 		vector();
 		explicit vector(const std::size_t count);
 		~vector();
+
+		typedef T * iterator;
+		typedef const T * const_iterator;
+		
+		iterator		begin();
+		const_iterator	begin() const;
+		iterator		end();
+		const_iterator	end() const;
+	
+	private:
+		T			*array;
+		iterator	it_first;
+		iterator	it_last;
+	
 	};
 }
 
+//CONSTRUCT
 template<typename T>
-ft::vector<T>::vector(const std::size_t count) : array(new T[count]){}
+ft::vector<T>::vector() : array(new T[10]), it_first(array), it_last(array + 10){}
+
+template<typename T>
+ft::vector<T>::vector(const std::size_t count) :	array(new T[count]),
+													it_first(array),
+													it_last(array + count){}
+
+template<typename T>
+ft::vector<T>::~vector(){
+	delete[] array;
+}
+
+//ITERATORS
+template<typename T>
+typename ft::vector<T>::iterator	ft::vector<T>::begin(){
+	return (it_first);
+}
+
+template<typename T>
+typename ft::vector<T>::iterator	ft::vector<T>::end(){
+	return (it_last);
+}
 
 #endif
