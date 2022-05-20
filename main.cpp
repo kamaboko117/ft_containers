960ada3762b6882bac9dc794ebabb86a6a70d7bb
Alexander SABOURET <asaboure@e1r11p19.clusters.42paris.fr>
2022-05-20 18:10:39 +0200

    working on vector

--- main.cpp
+++ main.cpp
@@ -6,15 +6,17 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 13:43:58 by asaboure          #+#    #+#             */
-/*   Updated: 2022/05/20 15:26:42 by asaboure         ###   ########.fr       */
+/*   Updated: 2022/05/20 16:45:42 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <vector>
 #include "vector.hpp"
 #include <iostream>
+#include <cstdlib>
 
 int	main(){
+	srand(time(0));
 	int	i = 0;
 	
 	std::vector<int>	vec;
@@ -23,10 +25,14 @@ int	main(){
 	std::vector<int>	vec2(4);
 	ft::vector<int>		ft_vec2(4);
 
-	for (std::vector<int>::iterator it = vec2.begin(); it != vec2.end(); it++)
-		std::cout << i++ << std::endl;
+	for (std::vector<int>::iterator it = vec2.begin(); it != vec2.end(); it++){
+		vec2[i++] = std::rand();
+		std::cout << *it << std::endl;
+	}
 	i = 0;
 	std::cout << std::endl;
-	for (ft::vector<int>::iterator it = ft_vec2.begin(); it != ft_vec2.end(); it++)
-		std::cout << i++ << std::endl;
+	for (ft::vector<int>::iterator it = ft_vec2.begin(); it != ft_vec2.end(); it++){
+		ft_vec2[i++] = std::rand();
+		std::cout << *it << std::endl;
+	}
 }
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 13:35:49 by asaboure          #+#    #+#             */
-/*   Updated: 2022/05/20 15:25:57 by asaboure         ###   ########.fr       */
+/*   Updated: 2022/05/20 18:10:09 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,49 +19,94 @@ namespace ft
 	class vector
 	{	
 	public:
+		typedef T * iterator;
+		typedef const T * const_iterator;
+		
+	private:
+		T			*array;
+		iterator	it_first;
+		iterator	it_end;
+		iterator	it_last;
+		std::size_t	_capacity;
+		std::size_t	_size;
+	public:
 		vector();
 		explicit vector(const std::size_t count);
 		~vector();
 
-		typedef T * iterator;
-		typedef const T * const_iterator;
+		vector	&operator=(vector const &rhs);
+		T		&operator[](std::size_t);
 		
 		iterator		begin();
 		const_iterator	begin() const;
 		iterator		end();
 		const_iterator	end() const;
-	
-	private:
-		T			*array;
-		iterator	it_first;
-		iterator	it_last;
-	
+		std::size_t		capacity() const;
+		std::size_t		size() const;
 	};
-}
 
-//CONSTRUCT
-template<typename T>
-ft::vector<T>::vector() : array(new T[10]), it_first(array), it_last(array + 10){}
+	//CONSTRUCT
+	template<typename T>
+	vector<T>::vector() : 	array(new T[10]),
+							it_first(array),
+							it_end(array),
+							it_last(array + 10),
+							_capacity(10),
+							_size(0){}
 
-template<typename T>
-ft::vector<T>::vector(const std::size_t count) :	array(new T[count]),
+	template<typename T>
+	vector<T>::vector(const std::size_t count) :	array(new T[count]),
 													it_first(array),
-													it_last(array + count){}
+													it_end(array + count),
+													it_last(array + count),
+													_capacity(count),
+													_size(count){}
 
-template<typename T>
-ft::vector<T>::~vector(){
-	delete[] array;
+	template<typename T>
+	vector<T>::~vector(){
+		delete[] array;
+	}
+
+	//OPERATORS
+	template<typename T>
+	vector<T>	&vector<T>::operator=(vector const &rhs){
+		if (this == &rhs)
+			return (*this);
+		this->~vector();
+		new(this) vector(rhs.capacity());
+		it_first = array;
+		it_end = array + rhs.capacity();
+		it_last = array + rhs.size();
+		for (size_t i = 0; i < size(); i++)
+			array[i] = rhs[i];
+		return (*this);
+	}
+	
+	template<typename T>
+	T	&vector<T>::operator[](std::size_t i){
+		return (array[i]);
+	}
+
+	//ITERATORS
+	template<typename T>
+	typename vector<T>::iterator	vector<T>::begin(){
+		return (it_first);
+	}
+
+	template<typename T>
+	typename vector<T>::iterator	vector<T>::end(){
+		return (it_end);
+	}
+
+	//ETC
+	template<typename T>
+	std::size_t	vector<T>::capacity() const{
+		return (_capacity);
+	}
+
+	template<typename T>
+	std::size_t	vector<T>::size() const{
+		return (_size);
+	}
 }
-
-//ITERATORS
-template<typename T>
-typename ft::vector<T>::iterator	ft::vector<T>::begin(){
-	return (it_first);
-}
-
-template<typename T>
-typename ft::vector<T>::iterator	ft::vector<T>::end(){
-	return (it_last);
-}
-
 #endif
