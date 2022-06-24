b09964867d8c7ea24a5642d3077be4abf0001c5b
Alexander SABOURET <asaboure@e2r2p19.42.fr>
2022-06-24 17:04:50 +0200

    saucisse

--- main_map.cpp
+++ main_map.cpp
@@ -6,7 +6,7 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 19:54:06 by asaboure          #+#    #+#             */
-/*   Updated: 2022/06/24 15:29:13 by asaboure         ###   ########.fr       */
+/*   Updated: 2022/06/24 15:57:25 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -132,6 +132,21 @@ struct classcomp {
 	}
 };
 
+typedef NAME::map<T1, T2> _map;
+typedef _map::const_iterator const_it;
+
+static unsigned int i = 0;
+
+void	ft_comp(const _map &mp, const const_it &it1, const const_it &it2)
+{
+	bool res[2];
+
+	std::cout << "\t-- [" << ++i << "] --" << std::endl;
+	res[0] = mp.key_comp()(it1->first, it2->first);
+	res[1] = mp.value_comp()(*it1, *it2);
+	std::cout << "with [" << it1->first << " and " << it2->first << "]: ";
+	std::cout << "key_comp: " << res[0] << " | " << "value_comp: " << res[1] << std::endl;
+}
 
 //check bst allocation
 int	main(){
@@ -521,6 +536,23 @@ int	main(){
 		mp.upper_bound(7)->second = 842;
 		ft_bound(mp, 5);
 		ft_bound(mp, 7);
+
+		printSize(mp);
+	}
+
+	
+	{
+		NAME::map<char, foo<float> >	mp;
+
+		mp['a'] = 2.3;
+		mp['b'] = 1.4;
+		mp['c'] = 0.3;
+		mp['d'] = 4.2;
+		printSize(mp);
+
+		for (const_it it1 = mp.begin(); it1 != mp.end(); ++it1)
+			for (const_it it2 = mp.begin(); it2 != mp.end(); ++it2)
+				ft_comp(mp, it1, it2);
 
 		printSize(mp);
 	}
